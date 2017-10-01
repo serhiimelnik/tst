3b9385a5ee117b0da7949302476e869b4f6b922d
Serhii Melnyk <serhiimelnik@gmail.com>
2017-10-01 16:09:38 +0300

    Add error n2o_async

--- apps/sample/src/sample_common.erl
+++ apps/sample/src/sample_common.erl
@@ -3,7 +3,7 @@
 -include("sample.hrl").
 
 %% API
--export([reg/2]).
+-export([reg/2, resend/1, verify/2]).
 
 reg(Token, Phone) ->
   io:format("Phone: ~p~n", [Phone]),
@@ -22,3 +22,38 @@ reg(Token, Phone) ->
   io:format("AUTH REG SMS: ~p~n", [{Token, Code}]),
 
   {ok,sms_sent}.
+
+resend(Token) ->
+  case kvs:get('Auth', Token) of
+    {ok, #'Auth'{}=Auth} ->
+      Auth1 = Auth#'Auth'{type=resend},
+      kvs:put(Auth1),
+      io:format("AUTH  RESEND  OK:  ~p~n", [Auth1]),
+      {ok,sms_sent};
+    {error, _} ->
+      io:format("AUTH  RESEND  SESSION  NOT  FOUND"),
+      {error,session_not_found}
+  end.
+
+verify(Token, Code) ->
+  case kvs:get('Auth', Token) of
+    {ok, #'Auth'{sms_code=Code1, attempts=Attempts}=Auth} when Attempts > 0 ->
+      case Code1 =:= Code of
+        true  ->
+          kvs:put(Auth#'Auth'{type=verify}),
+          io:format("AUTH  VERIFY  SMS:  ~p~n", [Auth]),
+          {ok,verifyed};
+        false ->
+          update_attempts(Auth),
+          {error,invalid_code}
+      end;
+    _ -> {error,session_not_found}
+  end.
+
+update_attempts(#'Auth'{attempts=Attempts}=Auth) when Attempts > 0 ->
+  Attempts1 = Attempts - 1,
+  kvs:put(Auth#'Auth'{attempts=Attempts1}),
+  n2o_async:send("looper", Attempts1);
+update_attempts(#'Auth'{token = Token}) ->
+  kvs:delete('Auth', Token),
+  wf:redirect("/").
--- apps/sample/src/verify.erl
+++ apps/sample/src/verify.erl
@@ -9,10 +9,10 @@ main()  ->
 
 body() ->
   [ #span   { id=display },    #br{},
-    #span   { id=attempts, body="Attempts: "},
-    #br{},
+    #span   { id=attempts, body="Attempts: "}, #br{},
     #span   { body="Code: "},  #number{id=code,autofocus=true},
     #button { id=verifyButton, body="Verify",postback=verify,source=[code]}, #br{},
+    #span   { id=error, body=""}, #br{},
     #button { id=resendButton, body="Resend",postback=resend} ].
 
 event(init) ->
@@ -20,45 +20,32 @@ event(init) ->
     undefined -> wf:redirect("/");
     _         ->
       wf:async("looper",fun verify:loop/1),
+      wf:async("errorLoop",fun verify:error_loop/1),
       {ok, Auth} = kvs:get('Auth', n2o_session:session_id()),
       n2o_async:send("looper", Auth#'Auth'.attempts),
       ok
   end;
 event(resend) ->
-  case kvs:get('Auth', n2o_session:session_id()) of
-    {ok, #'Auth'{}=Auth} ->
-      Auth1 = Auth#'Auth'{type=resend},
-      kvs:put(Auth1),
-      io:format("AUTH  RESEND  OK:  ~p~n", [Auth1]);
-    {error, _} ->
-      io:format("AUTH  RESEND  SESSION  NOT  FOUND"),
-      wf:redirect("/")
+  case sample_common:resend(n2o_session:session_id()) of
+    {ok,sms_sent}             ->
+      n2o_async:send("errorLoop", "Resended");
+    {error,session_not_found} -> wf:redirect("/")
   end;
 event(verify) ->
   Code = wf:q(code),
-  case kvs:get('Auth', n2o_session:session_id()) of
-    {ok, #'Auth'{sms_code=Code1, attempts=Attempts}=Auth} when Attempts > 0 ->
-      case Code1 =:= Code of
-        true  ->
-          kvs:put(Auth#'Auth'{type=verify}),
-          io:format("AUTH  VERIFY  SMS:  ~p~n", [Auth]),
-          wf:redirect("/login");
-        false ->
-          update_attempts(Auth)
-      end;
-    _ -> wf:redirect("/")
+  case sample_common:verify(n2o_session:session_id(), Code) of
+    {ok,verifyed}             -> wf:redirect("/login");
+    {error,invalid_code}      -> n2o_async:send("errorLoop", "Error: Invalid code");
+    {error,session_not_found} -> wf:redirect("/")
   end;
 event(E) -> wf:info(?MODULE,"Unknown Event: ~p~n",[E]).
-
-update_attempts(#'Auth'{attempts=Attempts}=Auth) when Attempts > 0 ->
-  Attempts1 = Attempts - 1,
-  kvs:put(Auth#'Auth'{attempts=Attempts1}),
-  n2o_async:send("looper", Attempts1);
-update_attempts(#'Auth'{token = Token}) ->
-  kvs:delete('Auth', Token),
-  wf:redirect("/").
 
 loop(M) ->
   DTL = #dtl{file="attempts",app=sample,bindings=[{attempts,M}]},
   wf:update(attempts, wf:jse(wf:render(DTL))),
+  wf:flush().
+
+error_loop(M) ->
+  DTL = #dtl{file="errors",app=sample,bindings=[{error_msg,M}]},
+  wf:update(error, wf:jse(wf:render(DTL))),
   wf:flush().
